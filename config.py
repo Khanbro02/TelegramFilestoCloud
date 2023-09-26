ea6dd5295c3979f2b861f1c8e5f4424634e8aa8f
Khanbro02 <143580678+Khanbro02@users.noreply.github.com>
2023-09-26 19:49:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    BOT_USE = False
+    BOT_USE = True
     BOT_TOKEN = '6451532210:AAG9CtqX3rjXMyFtnN1mZUe3_UTHjHyHmto'                         # from @botfather
-    APP_ID = 25952501                          # from https://my.telegram.org/apps
+    APP_ID = '25952501'                         # from https://my.telegram.org/apps
     API_HASH = 'b3d35d25998aae0a80c07175203435f9'                          # from https://my.telegram.org/apps
     AUTH_USERS = [6673836988]
